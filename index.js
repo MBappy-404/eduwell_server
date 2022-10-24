21d9b933baf6255a5ee8580922dfe1652a6984a2
MBappy-404 <muhammadbappy989@gmail.com>
2022-10-24 22:44:15 +0530

    added course details

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 const cors = require('cors');
+app.use(cors());
 
-
+const categories = require('./Data/categories.json');
 
 app.get('/', (req, res) => {
      res.send('EduWell Server is Running!')
 })
 
-app.use(cors());
+app.get('/course-categories', (req, res) =>{
+
+     res.send(categories);
+})
+
+
+
+
 
 
 
