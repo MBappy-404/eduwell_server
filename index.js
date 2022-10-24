8f88f5fc3c4f8b0e8581d352e36bb706c3e2215d
MBappy-404 <muhammadbappy989@gmail.com>
2022-10-24 23:39:20 +0530

    added data on server

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const port = process.env.port || 5000;
 const cors = require('cors');
 app.use(cors());
 
-const categories = require('./Data/categories.json');
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
      res.send('EduWell Server is Running!')
 })
 
-app.get('/course-categories', (req, res) =>{
-
+app.get('/course-categories', (req, res) => {
      res.send(categories);
 })
 
+app.get('/course-details', (req, res) =>{
+     res.send(courses);
+ });
 
-
+app.get('/course-details/:id', (req, res) => {
+     const id = req.params.id;
+     const getCourse = courses.find( course => course.id === id);
+     res.send(getCourse);
+})
 
 
 
