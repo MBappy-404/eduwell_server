8c81f4f98e6b1ca4ab318c4f116db5c0042150e0
MBappy-404 <muhammadbappy989@gmail.com>
2022-10-24 21:13:23 +0530

    added eduwell server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+const cors = require('cors');
+
+
+
+app.get('/', (req, res) => {
+     res.send('EduWell Server is Running!')
+})
+
+app.use(cors());
+
+
+
+
+app.listen(port, () => {
+     console.log(`Example app listening on port ${port}`)
+})
